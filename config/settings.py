4995b29b6657660ed4de5901b56776d627f80a4a
Akram Alisher <a.akram@infin-it.kz>
2025-07-04 13:03:47 +0500

    feat: csrf trusted origins

--- config/settings.py
+++ config/settings.py
@@ -187,5 +187,9 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
-
 LOGIN_URL = '/'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://tbexpert.kz",          # no trailing slash
+    "https://www.tbexpert.kz",      # if www used too
+]
